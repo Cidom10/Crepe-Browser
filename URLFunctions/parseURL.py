7ada7beb3f401dcd5ff285563476012aefd348f5
ljk005 <logan.j.knies@gmail.com>
2022-04-26 17:56:58 -0500

    QueryStringfunction

--- URLFunctions/parseURL.py
+++ URLFunctions/parseURL.py
@@ -1,3 +1,17 @@
 # Does the creation of a url that looks like
 # https://google.com/Hi%20There%p=""
-console.log("hi")
+import requests
+
+
+userinputtest = 'spiders bugs'
+
+#console.log("hi")
+def createurl(userinput):
+    search = 'https://www.google.com/search'
+    usersearch = {"q": userinput}
+    resp = requests.get(search, params=usersearch)
+    final = str(resp.url)
+    return final 
+
+t1 = createurl(userinputtest)
+print(t1)
